69f0d8e3ac3f11c5c7c1374d68d4330742e74fd4
yadir-vega-NTT <heimar.yadir.vega.bernal@nttdata.com>
2024-09-09 20:11:33 -0500

    Ajustes a notebook

--- Labs/RaizCuadrada.py
+++ Labs/RaizCuadrada.py
@@ -1,15 +1,10 @@
-def calcular_raiz_cuadrada(numero):
+def raiz_cuadrada(numero):
     if numero < 0:
-        return "Un número negativo no tiene raíz cuadrada real."
-    
-    aproximacion = numero / 2.0
-    tolerancia = 0.000001
-    while True:
-        mejor_aproximacion = (aproximacion + numero / aproximacion) / 2.0
-        if abs(aproximacion - mejor_aproximacion) < tolerancia:
-            return mejor_aproximacion
-        aproximacion = mejor_aproximacion
+        return "Error: No se puede calcular la raíz cuadrada de un número negativo."
+    else:
+        return numero ** 0.5
 
-numero = float(input("Ingrese un número: "))
-resultado = calcular_raiz_cuadrada(numero)
-print(f"La raíz cuadrada de {numero} es: {resultado}")
+# Ejemplo de uso
+numero = float(input("Introduce un número: "))
+resultado = raiz_cuadrada(numero)
+print(f"La raíz cuadrada de {numero} es {resultado}")
